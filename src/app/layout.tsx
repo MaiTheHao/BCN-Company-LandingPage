5ade314f01287fb7022338bf60c0a114b4804fed
Neo Adrian <134072533+MaiTheHao@users.noreply.github.com>
2025-08-01 19:22:26 +0700

    Reorder imports in layout.tsx for consistency and clarity

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,8 @@
 import './globals.scss';
+import { metadata } from './root-metadata';
 import { montserrat, open_sans } from './fonts';
-import Footer from '@/components/layout/Footer/Footer';
 import Header from '@/components/layout/Header/Header';
+import Footer from '@/components/layout/Footer/Footer';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
 	return (
@@ -16,3 +17,9 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
 		</html>
 	);
 }
+
+export { metadata };
+export const viewport = {
+	width: 'device-width',
+	initialScale: 1,
+};
