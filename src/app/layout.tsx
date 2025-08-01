f14e9da1192ff9453dba7bd663087ea8e0b66c9d
Neo Adrian <134072533+MaiTheHao@users.noreply.github.com>
2025-08-01 18:44:31 +0700

    .

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,8 @@ import type React from 'react';
 import './globals.scss';
 import { metadata } from './root-metadata';
 import { montserrat, open_sans } from './fonts';
-import Header from '@/components/layout/Header/Header';
-import Footer from '@/components/layout/Footer/Footer';
+import Header from '../components/layout/Header/Header';
+import Footer from '../components/layout/Footer/Footer';
 
 export const viewport = {
 	width: 'device-width',
